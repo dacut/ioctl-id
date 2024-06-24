c3d37f7bdad6e81ff87f4b16b4858274da2a19b8
David Cuthbert <dacut@kanga.org>
2024-06-24 07:23:02 +0000

    Whitespace fixes from cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -2,32 +2,32 @@
 //!
 //! Currently, this only supports Linux. The long term goal is to support `ioctl` identifiers for other Unix-like
 //! operating systems, including FreeBSD and macOS.
-//! 
+//!
 //! ## Usage Notes
 //! [`IoctlId`] is an alias for the type to pass into the `ioctl(2)` request. This is either a `u32` or `u64`, depending
 //! on the target OS and architecture.
-//! 
+//!
 //! The [`io()`], [`ior()`], [`iow()`], and [`iowr()`] functions take a type parameter, similar to the `_IO()`,
 //! `_IOR()`, `_IOW()`, and `_IOWR()` macros.
-//! 
+//!
 //! For example, the following C code:
 //! ```c
 //! struct my_ioctl_data {
 //!     unsigned int a;
 //! };
-//! 
+//!
 //! #define MY_IOCTL _IOR(0x12, 0x34, struct my_ioctl_data)
 //! ```
-//! 
+//!
 //! Would be written in Rust as:
 //! ```rust
 //! use ioctl_id::*;
-//! 
+//!
 //! #[repr(C)]
 //! struct MyIoctlData {
 //!    a: u32,
 //! }
-//! 
+//!
 //! const MY_IOCTL: IoctlId = ior::<MyIoctlData>(0x12, 0x34);
 //! ```
 #![no_std]
